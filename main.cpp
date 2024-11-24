d5b0550497162b8bdd7a725b408a4859be107dd4
und3rwater <slopal777@gmail.com>
2024-11-24 18:35:22 +0500

    initial frame

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <string>
+#include "readFromFile.cpp"
+#include "writeToFile.cpp"
+#include "display.cpp"
+
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    const string inputFilename = "input.txt";
+    const string outputFilename = "output.txt";
+
+    vector<string> lines = readFromFile(inputFilename);
+
+    display(lines);
+
+    writeToFile(outputFilename, lines);
+
     return 0;
 }
