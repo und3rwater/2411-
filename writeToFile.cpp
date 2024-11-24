915267350f2dc1fe806b44281332799ccf7d4ca4
user <surniki622@gmail.com>
2024-11-24 19:09:44 +0500

    writeToFile error check

--- writeToFile.cpp
+++ writeToFile.cpp
@@ -8,6 +8,11 @@ using namespace std;
 void writeToFile(const string& filename, const vector<string>& lines) {
     ofstream file(filename);
 
+    if (!file) {
+        cerr << "Error" << filename << endl;
+        return;
+    }
+
     for (const auto& line : lines) {
         file << line << endl;
     }
