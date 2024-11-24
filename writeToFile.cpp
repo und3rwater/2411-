78d188392661e7d286884455605b1ff74ace0bbd
user <surniki622@gmail.com>
2024-11-24 19:08:59 +0500

    writeToFile

--- writeToFile.cpp
+++ writeToFile.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 void writeToFile(const string& filename, const vector<string>& lines) {
+    ofstream file(filename);
 
+    for (const auto& line : lines) {
+        file << line << endl;
+    }
+    file.close();
 }
